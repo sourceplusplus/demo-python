0c1e7874de5334cb625d8ef4ddd9b0ff8be51704
Brandon Fergerson <bfergerson@apache.org>
2023-03-06 23:06:31 +0400

    build: bump version to 0.1.9

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 from setuptools import setup
 
 setup(name='sourceplusplus',
-      version='0.1.8',
+      version='0.1.9',
       description='Source++ Python Demo',
       url='https://github.com/sourceplusplus/demo-python',
       author='Source++',
       author_email='hello@sourceplus.plus',
       license='Apache License, Version 2.0',
       packages=setuptools.find_packages(),
-      install_requires=['sourceplusplus>=0.1.8'])
+      install_requires=['sourceplusplus==0.1.9'])
